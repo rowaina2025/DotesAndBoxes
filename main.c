f33702a65c274683ddde4994791b729ba0b3c738
MariaBassem123 <96719636+MariaBassem123@users.noreply.github.com>
2022-01-05 18:47:18 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,17 +2,33 @@
 #include <string.h>
 #include <string.h>
 #include <ctype.h>
+#include<time.h>
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_YELLOW "\x1b[33m"
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN "\x1b[36m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
-#include"game_functions.h"
+#include"mainFuction.h"
+
+ struct Player{
+ int score1;
+ int score2;
+ int moves1;
+ int moves2;
+ int remLines;
+ char name1[20];
+ char name2[20];
+ int arr[12][12];
+ int turn[40];
+ int rank1;
+ int rank2; //time_score
+};
 
 
 int main()
 {
+    struct Player ply[40];
     Interface1();
     system("COLOR");
     return 0;
