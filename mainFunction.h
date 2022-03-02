297619e2d63e4a58c485174f2a0be36b024bdb69
Rokii <96254289+rowaina2025@users.noreply.github.com>
2022-03-02 18:38:10 +0200

    Create mainFunction.h

---
+++ mainFunction.h
@@ -0,0 +1,10 @@
+#ifndef GAME_FUNCTIONS_H_INCLUDED
+#define GAME_FUNCTIONS_H_INCLUDED
+
+
+void onePlayerMood(int n, int trn, int ld);
+void twoPlayerMood(int n,int trn, int ld);
+
+
+
+#endif // GAME_FUNCTIONS_H_INCLUDED
