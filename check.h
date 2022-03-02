9f3f54da8641eae6aab1f62c081c72e171900be4
Rokii <96254289+rowaina2025@users.noreply.github.com>
2022-03-02 18:36:58 +0200

    Create check.h

---
+++ check.h
@@ -0,0 +1,7 @@
+#ifndef CHECK_H_INCLUDED
+#define CHECK_H_INCLUDED
+
+int check1(int row,int col,int n,int A[2*n+2][2*n+2],int player);
+int check2(int row,int col,int n,int A[2*n+2][2*n+2],int player);
+
+#endif // CHECK_H_INCLUDED
