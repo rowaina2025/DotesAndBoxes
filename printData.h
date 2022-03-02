7538b5143145c94e259cc683e257efcd3ea276e9
Rokii <96254289+rowaina2025@users.noreply.github.com>
2022-03-02 18:40:13 +0200

    Create printData.h

---
+++ printData.h
@@ -0,0 +1,8 @@
+#ifndef PRINTDATA_H_INCLUDED
+#define PRINTDATA_H_INCLUDED
+
+void printGrid(int n,int A[2*n+2][2*n+2]);
+int check1(int row,int col,int n,int A[2*n+2][2*n+2],int player);
+
+
+#endif // PRINTDATA_H_INCLUDED
