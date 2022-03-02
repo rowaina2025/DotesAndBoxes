ce44fe1d3ddd631592dc6c52e57f7c37fd80f1b3
Rokii <96254289+rowaina2025@users.noreply.github.com>
2022-03-02 18:41:00 +0200

    Create saveLoad.h

---
+++ saveLoad.h
@@ -0,0 +1,9 @@
+#ifndef SAVELOAD_H_INCLUDED
+#define SAVELOAD_H_INCLUDED
+
+void save(int n, int *A, int trn,int t3);
+void load();
+void debug(int n,int A[2*n+2][2*n+2]);
+
+
+#endif // SAVELOAD_H_INCLUDED
